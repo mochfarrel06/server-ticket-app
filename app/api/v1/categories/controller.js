6aaa49fbfe6942401d09ed6ea07688e025f0967c
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-26 02:51:09 +0700

    add function get all categories

--- app/api/v1/categories/controller.js
+++ app/api/v1/categories/controller.js
@@ -1,5 +1,18 @@
 const Categories = require("./model");
 
+// Function getAll categories
+const index = async (req, res, next) => {
+  try {
+    const result = await Categories.find();
+    res.status(200).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Function create
 const create = async (req, res, next) => {
   try {
     const {name} = req.body;
@@ -12,4 +25,4 @@ const create = async (req, res, next) => {
   }
 };
 
-module.exports = {create};
+module.exports = {index, create};
--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,23 +1,8 @@
 const express = require("express");
 const router = express();
-const {create} = require("./controller");
+const {index, create} = require("./controller");
 
-router.get("/categories", (req, res) => {
-  const data = [
-    {
-      _id: 1,
-      name: "Seminar",
-    },
-    {
-      _id: 2,
-      name: "Workshop",
-    },
-  ];
-
-  res.status(201).json({
-    data,
-  });
-});
+router.get("/categories", index);
 
 router.post("/categories", create);
 
